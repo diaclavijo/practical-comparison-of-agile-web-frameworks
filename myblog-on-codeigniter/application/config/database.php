4b9354837dc4ab85bf6142a5d0e4a34f3ce7039b
Diaclavijo <lantius@fulgor.(none)>
2013-10-30 16:55:36 +0000

    addded programming-exercises

--- myblog-on-codeigniter/application/config/database.php
+++ myblog-on-codeigniter/application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = 'nemesis';
+$db['default']['password'] = '123456';
 $db['default']['database'] = 'myblog';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
--- myblog-on-codeigniter/application/views/post/index.php
+++ myblog-on-codeigniter/application/views/post/index.php
@@ -7,9 +7,11 @@ echo $pagination;
 ?>
 
 
-<h1> Your blog : <?= $this->tank_auth->get_username(); ?>'s blog </h1>
 
 
-<? if (isset($his_posts)) foreach ($his_posts as $post){ ?>
-	<? echo show_post($post) ?>
-<? }
+ <?php if ($this->tank_auth->is_logged_in()){ ?>
+	<h1> Your blog : <?= $this->tank_auth->get_username(); ?>'s blog </h1>
+	<? if (isset($his_posts)) foreach ($his_posts as $post){ ?>
+		<? echo show_post($post) ?>
+	<? } ?>
+<? } ?>
